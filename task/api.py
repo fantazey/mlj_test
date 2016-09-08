545dcd62144c5d5f3f5783e16dd206b33237e99e
Andrey Dyatlov <fantazey@gmail.com>
2016-09-09 01:17:05 +0700

    added jsonapi support

--- task/api.py
+++ task/api.py
@@ -1,21 +1,30 @@
+from datetime import datetime
 from task import app, models, db
 from flask import jsonify, request
+# todo: handle errors
+
 
 @app.route('/api/departments', methods=['GET'])
 def list_of_departments():
     depts = models.Departments.query.all()
-    return jsonify(depts)
+    data, errs = models.DepartmentsSchema(many=True).dump(depts)
+    return jsonify(data)
+
 
 @app.route('/api/employees', methods=['GET'])
 def list_of_employees():
+    start = datetime.now()
     emps = models.Employees.query.all()
-    return jsonify(emps)
+    data, errs = models.EmployeesSchema(many=True).dump(emps)
+    end = datetime.now()
+    print end - start
+    return jsonify(data)
 
 
 @app.route('/api/employees', methods=['POST'])
 def add_employees():
     emp = models.Employees(
-        emp_no=request.json.get('emp_no'),
+        id=request.json.get('id'),
         birth_date=request.json.get('birth_date'),
         hire_date=request.json.get('hire_date'),
         first_name=request.json.get('first_name'),
@@ -23,21 +32,27 @@ def add_employees():
         gender=request.json.get('gender')
     )
     db.session.add(emp)
+    db.session.commit()
     dept_emp = models.DeptEmp(
-        emp_no=emp.emp_no,
+        emp_no=emp.id,
         dept_no=request.json.get('dept_no'),
-        from_date=emp.hire_date,
-        to_date=emp.hire_date
+        from_date=request.json.get('hire_date'),
+        to_date=request.json.get('hire_date')
     )
     db.session.add(dept_emp)
     db.session.commit()
+    emp_ = models.Employees.query.filter(models.Employees.id==request.json.get('id'))
+    data, err = models.EmployeesSchema(many=True).dump(emp_)
+    return jsonify(data)
 
 
 @app.route('/api/departments', methods=["POST"])
 def add_dept():
     dept = models.Departments(
-        dept_no=request.json.get('dept_no'),
+        id=request.json.get('id'),
         dept_name=request.json.get('dept_name')
     )
     db.session.add(dept)
     db.session.commit()
+    return jsonify({
+    })
--- task/models.py
+++ task/models.py
@@ -1,37 +1,41 @@
 from task import db
-from flask import jsonify
+from marshmallow_jsonapi import Schema, fields
 
 
 class Employees(db.Model):
     __tablename__ = 'employees'
-    emp_no = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column('emp_no', db.Integer, primary_key=True, nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
     hire_date = db.Column(db.Date, nullable=False)
     first_name = db.Column(db.VARCHAR(16), nullable=False)
     last_name = db.Column(db.VARCHAR(16), nullable=False)
     gender = db.Column(db.Enum, nullable=False)
 
-    def __repr__(self):
-        return jsonify({
-            'emp_no': self.emp_no,
-            'birth_date': self.birth_date,
-            'hire_date': self.hire_date,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'gender': self.gender
-        })
+
+class EmployeesSchema(Schema):
+    id = fields.Str(dump_only=True)
+    birth_date = fields.Date(required=True)
+    hire_date = fields.Date(required=True)
+    first_name = fields.Str(required=True)
+    gender = fields.Str()
+
+    class Meta:
+        type_ = 'employees'
 
 
 class Departments(db.Model):
     __tablename__ = 'departments'
-    dept_no = db.Column(db.CHAR(4), primary_key=True, nullable=False)
+    id = db.Column('dept_no', db.CHAR(4), primary_key=True, nullable=False)
     dept_name = db.Column(db.Text, unique=True, nullable=False)
 
-    def __repr__(self):
-        return jsonify({
-            'dept_no': self.dept_no,
-            'dept_name': self.dept_name
-        })
+
+class DepartmentsSchema(Schema):
+    id = fields.Str(dump_only=True)
+    dept_name = fields.Str(required=True)
+
+    class Meta:
+        type_ = 'departments'
+
 
 class DeptEmp(db.Model):
     __tablename__ = 'dept_emp'
@@ -40,10 +44,12 @@ class DeptEmp(db.Model):
     from_date = db.Column(db.Date, nullable=False)
     to_date = db.Column(db.Date, nullable=False)
 
-    def __repr__(self):
-        return jsonify({
-            'emp_no': self.emp_no,
-            'dept_no': self.dept_no,
-            'from_date': self.from_date,
-            'to_date': self.to_date
-        })
+# todo: add relations
+class DeptEmpSchema(Schema):
+    id = fields.Str(dump_only=True)
+    dept_no = fields.Str(dump_only=True)
+    from_date = fields.Date()
+    to_date = fields.Date()
+
+    class Meta:
+        type_ = 'dept_emp'
